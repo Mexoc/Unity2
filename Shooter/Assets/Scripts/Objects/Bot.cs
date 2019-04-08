782f28b9e5570ef4ac3e1583c5aa3176aa9c91f5
Andrey <andreywtf@mail.ru>
2019-04-08 21:26:56 +0400

    карта навигации

---
+++ Shooter/Assets/Scripts/Objects/Bot.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Geekbrains.Objects
+{
+    public class Bot : Enemy
+    {        
+        private float _step = 1;
+        private float _speed = 1;
+
+        public void TakeDamage(float damage)
+        {
+            if (_hp > 0)
+            {
+                _hp -= damage;
+            }
+            if (_hp <= 0)
+            {
+                _hp = 0;
+                Color = Color.red;
+                _isDead = true;
+            }
+                
+        }
+
+        private void Update()
+        {
+            if (_isDead)
+            {
+                Color color = Color;
+                if (color.a > 0)
+                {
+                    color.a -= _step / 100;
+                    Color = color;
+                }
+                if (color.a < 1)
+                    Destroy(gameObject);
+            }               
+        }
+    }
+}
---
+++ Shooter/Assets/Scripts/Objects/Bullet.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Geekbrains.Objects
+{
+    public class Bullet : Ammunition
+    {        
+        [SerializeField] private float _timeToDestroy = 10; // Время жизни пули
+        [SerializeField] private float _damage = 20;     // Урон пули
+        [SerializeField] private float _mass = 0.01f;    // Масса пули        
+        private float _currentDamage; // Текущий урон пули
+
+        public override void Awake()
+        {
+            base.Awake();
+            // Если пуля не встретит ничего, то через заданное время пуля самоуничтожится
+            Destroy(_instanceObject, _timeToDestroy);
+            _currentDamage = _damage;
+            _rigidbody.mass = _mass;
+        }
+
+        // Если пуля встретила препятствие
+        private void OnCollisionEnter(Collision collision)
+        {  // Если столкнулись с другой пулей, то ничего не делаем
+            if (collision.collider.tag == "Bullet") return;
+            // Функция нанесения урона
+            SetDamage(collision.gameObject.GetComponent<ISetDamage>());
+            Destroy(_instanceObject);          // Удаляем пулю
+        }
+
+        private void SetDamage(ISetDamage damageObj)
+        {
+            damageObj.TakeDamage(_currentDamage);        
+        }
+    }
+}
+
